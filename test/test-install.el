b8709a77e9e3eee329dd88e0c6ae2e05509f462a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-27 11:34:58 +0800

    Add test

---
+++ test/test-install.el
@@ -0,0 +1,19 @@
+;;; test-install.el --- List out all package archives  -*- lexical-binding: t; -*-
+;;; Commentary: Test `quelpa' install capability with `leaf'
+;;; Code:
+
+(package-initialize)
+
+(require 'quelpa-leaf)
+(quelpa-leaf-init)
+
+;; test some installs
+(leaf grandshell-theme :quelpa)
+(leaf ipretty :quelpa t)
+(leaf flx-ido :quelpa (:stable t))
+(leaf flx-ido :quelpa ((flx-ido) :upgrade t))
+(leaf git-modes :quelpa (git-modes :fetcher github :repo "magit/git-modes"))
+(leaf git-timemachine :quelpa ((git-timemachine :fetcher gitlab :repo "pidu/git-timemachine") :stable t))
+(leaf git-timemachine :quelpa ((git-timemachine :fetcher gitlab :repo "pidu/git-timemachine") :upgrade t))
+
+;;; test-install.el ends here
