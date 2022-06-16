4dd07a39fe41eb748a5a9883fa44a74fee4e4729
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-17 01:59:06 +0800

    rev maintainer info

--- quelpa-leaf.el
+++ quelpa-leaf.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/quelpa/quelpa-leaf
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.1") (quelpa "1.0") (leaf "4.1.0"))
-;; Keyword: package managment elpa leaf
+;; Keywords: package managment elpa leaf
 
 ;; This file is NOT part of GNU Emacs.
 
